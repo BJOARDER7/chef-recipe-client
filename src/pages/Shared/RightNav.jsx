c9488abda57537eb0aeed87f38cda40f5d0fc1e7
BJoarder <bjoarder87@gmail.com>
2023-05-05 22:54:00 +0600

    signIn with google & github setup

--- src/pages/Shared/RightNav.jsx
+++ src/pages/Shared/RightNav.jsx
@@ -1,16 +1,44 @@
 import React from "react";
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { Button } from "react-bootstrap";
 import { FaGoogle, FaGithub } from "react-icons/fa";
 import Register from "../Home/Register";
+import app from "../../firebase/firebase.config";
 
 const RightNav = () => {
+
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+
+  const handleGoogleLogin = () => {
+    signInWithPopup(auth, provider)
+    .then(result => {
+      const user = result.user;
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  const handleGithubLogin = () => {
+    signInWithPopup(auth, githubProvider)
+    .then(result => {
+      const loggedUser = result.user;
+      console.log(loggedUser);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
   return (
     <div>
       <h4 className="mt-4">Login With</h4>
-      <Button className="mb-2" variant="outline-primary">
+      <Button onClick={handleGoogleLogin} className="mb-2" variant="outline-primary">
         <FaGoogle /> Login with Google
       </Button>
-      <Button variant="outline-secondary">
+      <Button onClick={handleGithubLogin} variant="outline-secondary">
         <FaGithub></FaGithub> Login with Github
       </Button>
       <Register></Register>
